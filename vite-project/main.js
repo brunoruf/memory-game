35405d592e495e814b06d7f57b0d9a75807e1961
Bruno Ruf <brunoruf@gmail.com>
2022-03-16 17:37:09 -0300

    Cartoes virando no clique (e desvirando) individualmente

--- vite-project/main.js
+++ vite-project/main.js
@@ -15,6 +15,6 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML("beforeend", 
     `
     ${ScoreBoard()}
-    ${BoardGame(2)}
+    ${BoardGame(6)}
     `
 )
--- vite-project/src/components/CardFrontBack/index.js
+++ vite-project/src/components/CardFrontBack/index.js
@@ -1,13 +1,32 @@
 import "./style.css"
 import CardTemplate from "../CardTemplate"
 
+
+
 const CardFrontBack = () => {
     return /*html*/`
-        <div class="card-front-back">
+        <div class="card-front-back" onClick="cardFrontBack.handleClick(event)">
             <div class="card-front">${CardTemplate()}</div>
             <div class="card-back">${CardTemplate('cuphead', 'CupHead')}</div>
-        </div>
-    `
+        </div>`
+
 }
+
+window.cardFrontBack = {}
+
+window.cardFrontBack.handleClick= (event) => {
+        const origin = event.target
+        const selectCard = origin.closest('.card-front-back')
+        /*
+        if (selectCard.classList.contains('active')) {
+            selectCard.classList.remove('active')
+        } else {
+            selectCard.classList.add('active')
+        }
+        O que está aqui embaixo \/ faz exatamente o mesmo que está comentado, verifica se tem e remove / se não tem adiciona
+        */
+        selectCard.classList.toggle('active')
+
+    }
 
 export default CardFrontBack
